8687a6f12296c9b1fb227a214ab3c9642c02ddeb
WilmerCamilo <xwilmerr11@gmail.com>
2023-05-21 19:09:00 -0500

    Ajustes Proyecto

--- frontend.py
+++ frontend.py
@@ -57,13 +57,39 @@ def actualizar_resultados(n_clicks, peso_total, peso_solidos, gravedad_especific
                 # Extraer los valores relevantes de los resultados
                 volumen_vacios = resultados['volumen_vacios']
                 volumen_aire = resultados['volumen_aire']
-                # Agrega más valores aquí según sea necesario
+                volumen_agua = resultados['volumen_agua']
+                volumen_particulas_solidas = resultados['volumen_particulas_solidas']
+                volumen_total = resultados['volumen_total']
+                peso_total = resultados['peso_humedo']
+                peso_agua = resultados['peso_agua']
+                peso_seco = resultados['peso_seco']
+                humedad = resultados['humedad']
+                relacion_vacios = resultados['relacion_vacios']
+                porosidad = resultados['porosidad']
+                peso_unitario_humedo = resultados['peso_unitario_humedo']
+                peso_unitario_particulas_solidas = resultados['peso_unitario_particulas_solidas']
+                peso_unitario_seco = resultados['peso_unitario_seco']
+                gravedad_especifica = resultados['gravedad_especifica']
+                saturacion_relativa = resultados['saturacion_relativa']
 
                 # Mostrar los resultados en el resultados-div
                 return [
                     html.P(f'Volumen vacíos: {volumen_vacios:.2f}m³'),
                     html.P(f'Volumen aire: {volumen_aire:.2f}m³'),
-                    # Agrega más componentes aquí según sea necesario
+                    html.P(f'Volumen agua: {volumen_agua:.2f}m³'),
+                    html.P(f'Volumen solidos: {volumen_particulas_solidas:.2f}m³'),
+                    html.P(f'Volumen total: {volumen_total:.2f}m³'),
+                    html.P(f'Peso húmedo: {peso_total:.2f}Kg'),
+                    html.P(f'Peso del agua: {peso_agua:.2f}Kg'),
+                    html.P(f'Peso seco: {peso_seco:.2f}Kg'),
+                    html.P(f'Humedad: {humedad:.2f}%'),
+                    html.P(f'Relación de vacios: {relacion_vacios:.2f}'),
+                    html.P(f'Porosidad: {porosidad:.2f}'),
+                    html.P(f'Peso unitario húmedo: {peso_unitario_humedo:.2f}Kg/m³'),
+                    html.P(f'Peso unitario de las particulas solidas: {peso_unitario_particulas_solidas:.2f}Kg/m³'),
+                    html.P(f'Peso unitario seco: {peso_unitario_seco:.2f}Kg/m³'),
+                    html.P(f'Gravedad especifica: {gravedad_especifica:.2f}'),
+                    html.P(f'Saturación relativa: {saturacion_relativa:.2f}%'),
                 ]
             else:
                 return html.P('Error: Verifique los valores ingresados.')
